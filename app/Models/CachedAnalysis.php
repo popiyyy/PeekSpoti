ba9ec6fb116ab7e5a8dd27070852f7e732440016
Farhan Khairullah <farhankhairullah174@gmail.com>
2026-04-27 13:59:31 +0700

    chore: db

--- app/Models/CachedAnalysis.php
+++ app/Models/CachedAnalysis.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class CachedAnalysis extends Model
 {
-    //
+    protected $fillable = [
+        'searched_user_id',
+        'top_artists_json',
+        'top_genres_json',
+        'expires_at',
+    ];
+
+    public function searchedUser()
+    {
+        return $this->belongsTo(SearchedUser::class);
+    }
 }
--- app/Models/SearchedUser.php
+++ app/Models/SearchedUser.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class SearchedUser extends Model
 {
-    //
+    protected $fillable = [
+        'spotify_username',
+        'display_name',
+        'avatar_url',
+        'total_public_playlists',
+    ];
+
+    public function cachedAnalysis()
+    {
+        return $this->hasOne(CachedAnalysis::class);
+    }
 }
