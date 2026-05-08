0066f8421d12072070a8ec3be9370aa0a518319a
Farhan Khairullah <farhankhairullah174@gmail.com>
2026-05-08 17:04:15 +0700

    fix:title dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SpotiPeek - Analisis Playlist Publik Spotify</title>
+    <title>SpotiPeek</title>
+    <link rel="icon" type="image/png" href="{{ asset('spotify.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-zinc-950 text-zinc-100 font-sans antialiased min-h-screen flex flex-col">
