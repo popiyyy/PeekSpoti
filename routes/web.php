0156f4683673737c6c360d59edbfc1696489dd6e
Farhan Khairullah <farhankhairullah174@gmail.com>
2026-07-02 20:47:42 +0700

    feat: cron job for still alive db

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,35 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use Illuminate\Http\Request; 
+use Illuminate\Support\Facades\DB;
 
 // Halaman Utama
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+// Cron Job Keep-Alive supabase 
+Route::get('/api/cron-keep-alive', function (Request $request){
+    // verif cron 
+    $cronSecret = env('CRON_SECRET');
+    if ($cronSecret && $request->header('Authorization') !== 'Bearer' . $cronSecret) {
+        return response()->json(['error' => 'Unauthorizated'], 401);
+    }
+
+    try { 
+        DB::select('SELECT 1');
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Database keep-alive ping sent success.'
+        ]);
+    } catch (\Exception $e) { 
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Database connection failed: ' . $e->getMessage()
+        ], 500);
+    }
+});
 
 // Autentikasi Spotify (Dibatasi 10 request per menit untuk mencegah abuse)
 Route::middleware('throttle:10,1')->group(function () {
